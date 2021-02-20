37a75f5f055c351d01dec200ac415227375753d5
tbasilealvarez <37352933+tbasilealvarez@users.noreply.github.com>
2021-02-20 14:11:11 -0600

    Update ejemplo.js

--- ejemplo.js
+++ ejemplo.js
@@ -1,55 +1,12 @@
-var x = 60; // x coordinate
-var y = 440; // y coordinate
-var radius = 45; // Head radius
-var bodyHeight = 180; // Body height
-var neckHeight = 40; // Neck height
-var easing = 0.04;
 function setup() {
- createCanvas(360, 480);
- strokeWeight(2);
- ellipseMode(RADIUS);
+ createCanvas(240, 120);
 }
 function draw() {
-
- var neckY = -1 * (bodyHeight + neckHeight + radius);
-
  background(204);
-
- translate(mouseX, y); // Move all to (mouseX, y)
-
- if (mouseIsPressed) {
- scale(1.0);
- } else {
- scale(0.6); // 60% size when mouse is pressed
+ for (var x = 20; x < width; x += 20) {
+ var mx = mouseX / 10;
+ var offsetA = random(-mx, mx);
+ var offsetB = random(-mx, mx);
+ line(x + offsetA, 20, x - offsetB, 100);
  }
-
- // Body
- noStroke();
- fill(102);
- ellipse(0, -33, 33, 33);
- fill(0);
- rect(-45, -bodyHeight, 90, bodyHeight-33);
- // Neck
- stroke(102);
- line(12, -bodyHeight, 12, neckY);
-
- // Hair
- push();
- translate(12, neckY);
- var angle = -PI/30.0;
- for (var i = 0; i <= 30; i++) {
- line(80, 0, 0, 0);
- rotate(angle);
- }
- pop();
-
- // Head
- noStroke();
- fill(0);
- ellipse(12, neckY, radius, radius);
- fill(255);
- ellipse(24, neckY-6, 14, 14);
- fill(0);
- ellipse(24, neckY-6, 3, 3);
-
 }
