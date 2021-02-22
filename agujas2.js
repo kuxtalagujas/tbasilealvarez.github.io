e24d7409cccff068da744a8b3b1eebab5e7c474f
tbasilealvarez <37352933+tbasilealvarez@users.noreply.github.com>
2021-02-22 11:28:43 -0600

    Update agujas2.js

--- agujas2.js
+++ agujas2.js
@@ -106,10 +106,10 @@ function Lineas(posx,posy,angu){
     text(buenos,480,540);
 
     text("Aproximación de Pi (2*N/n): ",280,560);
-    text(2*tot/buenos,480,560);
+    text(Math.round(2*tot/buenos,5),480,560);
 
     text("Porcentaje de error: ",280 , 580 );
-    text(abs(3.14159265-2*tot/buenos)/3.14159265*100,480,580 )
+    text(Math.round(abs(3.14159265-2*tot/buenos)/3.14159265*100,3),480,580 )
 
     text("%", 517,580);
     line(530,0,530,700);
